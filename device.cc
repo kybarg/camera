14b68d12726eac78806faaa9096600633213c3fb
Ruslan Kyba <kybargr@gmail.com>
2024-03-20 03:09:30 +0200

    chore: update

--- device.cc
+++ device.cc
@@ -147,6 +147,13 @@ HRESULT CaptureDevice::CreateStream() {
   hr = pMFTOutputMediaType->SetGUID(MF_MT_SUBTYPE, MFVideoFormat_RGB32);
   if (FAILED(hr)) goto CleanUp;
 
+  // Now adjust the subtype to reflect RGBA order
+  GUID rgb32Subtype = MFVideoFormat_RGB32;
+  rgb32Subtype.Data1 = MAKEFOURCC('R', 'G', 'B', 'A');
+
+  hr = pMFTOutputMediaType->SetGUID(MF_MT_SUBTYPE, rgb32Subtype);
+  if (FAILED(hr)) goto CleanUp;
+
   hr = pMFTOutputMediaType->SetUINT32(MF_MT_ALL_SAMPLES_INDEPENDENT, TRUE);
   if (FAILED(hr)) goto CleanUp;
 
